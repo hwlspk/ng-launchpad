cbb35b1aeceb65823954dfe7eb1ff918a2f43fd3
hwlspk <hwlspk@gmail.com>
2020-05-22 16:44:56 +0100

    Make explicit Angular compiler options

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ViewEncapsulation, MissingTranslationStrategy } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  defaultEncapsulation: ViewEncapsulation.Emulated,
+  missingTranslation: MissingTranslationStrategy.Warning,
+  preserveWhitespaces: false,
+  useJit: false
+})
   .catch(err => console.error(err));
